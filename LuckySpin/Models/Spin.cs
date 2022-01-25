0bf52a5984ea3eb49df145bef80b7cb3836ed9c2
roger21515 <roger21515@gmail.com>
2022-01-25 14:36:54 -0800

    Commit, Entity Framework Corrected

--- LuckySpin/Models/Spin.cs
+++ LuckySpin/Models/Spin.cs
@@ -4,9 +4,13 @@
     {
         public long SpinId { get; set; }
 
-        public bool Winner { get; set; }
+        public bool IsWinning { get; set; }
 
         public Player Player { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+
+        public int[] Numbers { get; set; }
 
 
     }
